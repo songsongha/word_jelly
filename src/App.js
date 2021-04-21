82be0264c7dfccaedec2de748189a02ad99ddc05
Mykel <songsongha@gmail.com>
2021-04-21 12:37:22 -0600

    added tachyons, created card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+constructor(){
+  super()
+  this.state = {
+
+  }
 }
-
+  render(){
+      return(
+        <div className = 'tc'>
+          <h1 className='f1'> Word Jelly </h1>
+          <div>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+          </div>
+          <br/>
+          <br/>
+          <div>
+              <Card/>
+          </div>
+        </div>
+      );
+  }
+}
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+		<div className= 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<div>
+				<h1>A</h1>
+				<p> Player #</p>
+			</div>
+		</div>
+		)
+}
+export default Card
