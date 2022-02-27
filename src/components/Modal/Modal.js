1c992e109ac5f2b6379347c4407a3974218c3d14
Song Ha <songsongha@gmail.com>
2022-02-27 09:02:06 -0700

    Change value format for clue form, add cancel button

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './modal.css'
 
 const Modal = ({show, onClose, G, playerID}) => {
+    const [formValues, setFormValues] = useState({})
+
     if (!show){
         return null
     }
     // drop down menu options for each player, plus wild option
     const dropDownOptions = []
     dropDownOptions.push(
-        <option key='blank' value='blank'>
+        <option name='' value=''>
         </option>
     )
     for (let player = 0; player < G.players.length; player++) {
 		if (player !== Number(playerID)){
 			dropDownOptions.push( 
-                <option key={player} value={G.words[player][G.players[player].letterPosition]}>
+                <option name={player} value={player}>
                     {G.words[player][G.players[player].letterPosition].toUpperCase()+' '}
                     (
                     {G.players[player].name}) 
@@ -23,22 +25,40 @@ const Modal = ({show, onClose, G, playerID}) => {
 		}
 	  }
     dropDownOptions.push(
-        <option key='wild' value='*'>
+        <option name='wild' value='*'>
             *   (wild)
         </option>
     )  
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setFormValues(values => ({...values, [name]: value}))
+      }
+
     const clueForm = []
     for (let i = 1; i <= 8; i++) {
         clueForm.push(
             <div>
                 Clue {i}
-                <select key={i}>
-                    {dropDownOptions}
+                <select 
+                    name={`clue${i}`} 
+                    value={formValues[`clue${i}`] || ''}
+                    onChange={handleChange}>
+                        {dropDownOptions}
                 </select>
             </div>
         )
     }  
-  
+    
+    const handleSubmit = () => {
+        console.log({formValues})
+        onClose()
+    }
+    const handleCancel = () => {
+        setFormValues({})
+        onClose()
+    }
     return (
         <div className='modal'>
             <div className='modal-content'>
@@ -46,10 +66,11 @@ const Modal = ({show, onClose, G, playerID}) => {
                     <h4 className='modal-title'>Give Clue</h4>
                 </header>
                 <main className='modal-body'>
-                    {clueForm}
+                        {clueForm}
                 </main>
                 <footer className='modal-footer'>
-                    <button className='button' onClick={onClose}>Close</button>
+                    <button className='button' onClick={handleCancel}>Cancel</button>
+                    <button className='button' onClick={handleSubmit}>Submit</button>
                 </footer>
             </div>
         </div>
