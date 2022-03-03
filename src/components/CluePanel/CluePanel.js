82731d6baa7ada513dda993e633512515f95e989
Song Ha <songsongha@gmail.com>
2022-03-03 10:08:18 -0700

    Only display clue if player is involved

--- src/components/CluePanel/CluePanel.js
+++ src/components/CluePanel/CluePanel.js
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const CluePanel = ({clues, playerID}) => {
-    let display = []
-    console.log({clues})
+const CluePanel = ({G, playerID}) => {
+    const display = []
+    const clues = G.clues
+    const cardPosition = G.players[playerID].letterPosition + 1
+    
     if (clues) {
-        console.log('clues exist')
-        console.log('clues.length', clues.length)
         for (let i = 0; clues[i]; i++) {
-            console.log('firs for loop triggered')
-            for(let j = 0; j < clues[i].length; j++){
-                console.log('second for loop triggered')
-                console.log('clues[i][j]', clues[i][j])
-                display.push(
-                    clues[i][j].player !== playerID ? clues[i][j].letter.toUpperCase() + ' ' : '? '
-                )
+            // only display the clue if the player is involved in the clue
+            if (clues[i].find(obj => obj.player === playerID)){
+                for(let j = 0; j < clues[i].length; j++){
+                    display.push(
+                        clues[i][j].player !== playerID ? clues[i][j].letter.toUpperCase() + ' ' : '? '
+                    )
+                }
+                display.push(' (Card ' + cardPosition + ')')
+                display.push(<br/>)
             }
-            display.push(<br/>)
         }
     }
     console.log({display})
