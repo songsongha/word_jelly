21b48e415b393a2b1dd9bcdaec14ecadc4c8c6a4
Song Ha <songsongha@gmail.com>
2022-03-12 11:27:16 -0700

    Changed nav style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,14 +4,14 @@ import logo from './logo.jpg';
 
 const Navigation = () => {
 	return(
-		<div>
-			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+		<div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'flex-between', marginBottom: '40px'}}>
+			<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'left', paddingLeft: '20px', width: '75%'}}>
+				<h1 className= 'title'> Word Jelly</h1>
+				<img src={logo} alt='Logo' width='100'/>
+			</div>
+			<nav style={{display: 'flex', justifyContent: 'flex-end', width: '25%'}}>
 				<a href="https://www.ultraboardgames.com/letter-jam/game-rules.php" target="_blank" rel="noreferrer" className='f3 link dim black underline pa3 pointer'>Rules</a>
 			</nav>
-			<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-				<h1 className= 'title f1'> Word Jelly</h1>
-				<img src={logo} alt='Logo' width='150'/>
-			</div>
 		</div>
 	);
 }
