dc8880d911d9c43b396bfd615c075ad1b6701e30
Song Ha <songsongha@gmail.com>
2022-03-31 23:08:28 -0600

    Add UseMemo to avoid unecessary rerendering

--- src/components/Board/Play/Play.js
+++ src/components/Board/Play/Play.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 import CardFaceUp from './Cards/CardFaceUp'
 import GiveClue from './Modal/GiveClue'
 import Reveal from './Modal/Reveal'
@@ -10,48 +10,51 @@ const Play= ({ctx, G, moves, playerID, events }) => {
     const {players, words, bonusLetters, permanentLetters} = G
     const [openModal, setOpenModal] = useState(false)
 	const [openGuessBonus, setOpenGuessBonus] = useState(false)
+    console.log('how often is play being rendered')
     
-    let cardRow = []
-	let wildCard = {
-		name: 'Wild'
-	}
-	
-	// show a letter from every player other than you
-	for (let player = 0; player < players.length; player++) {
-		if (player !== Number(playerID)){
-			let letter = words[player][players[player].letterPosition] || bonusLetters[player]
-
-			cardRow.push(
-				<CardFaceUp 
-				key = {`card${player}`}
-				letter = {letter}
-				player = {players[player]}
-				word = {words[player]} />
-			)
-		}
+    const cardRow = useMemo(()=>{
+        // show a letter from every player other than you
+        let cardRow = []
+        for (let player = 0; player < players.length; player++) {
+            if (player !== Number(playerID)){
+                let letter = words[player][players[player].letterPosition] || bonusLetters[player]
+                cardRow.push(
+                    <CardFaceUp 
+                    key = {`card${player}`}
+                    letter = {letter}
+                    player = {players[player]}
+                    word = {words[player]} />
+                )
+            }
 	  }
-	
-	// show permanent letters if applicable
-	const permanentLetterRow = []
-	if (permanentLetters && permanentLetters.length){
-		for (let letterIndex = 0; letterIndex < permanentLetters.length; letterIndex++){
-			if (permanentLetters && permanentLetters.length){
-				permanentLetterRow.push(
-					<CardFaceUp 
-					key = {`pLetter${letterIndex}`}
-					letter = {permanentLetters[letterIndex]}
-					player = 'bonus' />
-				)
-			}
-		}
-	}  
+    return cardRow  
+    },[bonusLetters, playerID, players, words])
+    
+    const permanentLetterRow = useMemo(()=>{
+        // show permanent letters if applicable
+        const permanentLetterRow = []
+        if (permanentLetters && permanentLetters.length){
+            for (let letterIndex = 0; letterIndex < permanentLetters.length; letterIndex++){
+                if (permanentLetters && permanentLetters.length){
+                    permanentLetterRow.push(
+                        <CardFaceUp 
+                        key = {`pLetter${letterIndex}`}
+                        letter = {permanentLetters[letterIndex]}
+                        player = 'bonus' />
+                    )
+                }
+            }
+        }
+        return permanentLetterRow
+    },[permanentLetters])  
+
 	return ( 
 		<div className='flex flex-row justify-between'>
 			<div className = 'tc pa4 w-75'>
 				<div> 
 					{cardRow}
 					<div>
-						<CardFaceUp letter='*' player ={wildCard}/>
+                        <CardFaceUp letter='*' player ={{name:'Wild'}}/>
 						{permanentLetterRow}
 					</div>
 					<br/>
