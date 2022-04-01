b393a2f2526df5fffe3feb3621f5a1fa43289242
Song Ha <songsongha@gmail.com>
2022-03-31 23:47:58 -0600

    Add useMemo to MyCards

--- src/components/Board/Play/MyCards/MyCards.js
+++ src/components/Board/Play/MyCards/MyCards.js
@@ -1,64 +1,75 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import CardFaceDown from '../Cards/CardFaceDown'
 import TokensTaken from '../CluePanel/TokenTracker/TokensTaken'
 
 
 const MyCards = ({G, playerID, setOpenModal, moves}) => {
-    const nextCard = () => {
-		moves.nextCard(playerID)
-	}
+    const {words, players, isNextCardAvailable, isClueInProgress, gameResults} = G
+    const letterPosition = players[playerID].letterPosition
 
-	const bonusLetter = () => {
-		if (G.players[playerID].letterPosition === G.words[playerID].length-1){
-			moves.nextCard(playerID)
-		} else {
-			setOpenModal(true)
-		}
-	} 
-    const letterPosition = G.players[playerID].letterPosition
-
-    let currentCard = ''
-    if (G){
-		if (letterPosition >= G.gameResults[playerID].word.length){
-			currentCard = 'Bonus'
-		} else {
-			currentCard = `Card ${letterPosition + 1}`
-		}
-	}
+    const current = useMemo(()=>{
+        const nextCard = () => {
+            moves.nextCard(playerID)
+        }
+    
+        const bonusLetter = () => {
+            if (players[playerID].letterPosition === words[playerID].length-1){
+                moves.nextCard(playerID)
+            } else {
+                setOpenModal(true)
+            }
+        } 
+        let currentCard = ''
+        if (G){
+            if (letterPosition >= gameResults[playerID].word.length){
+                currentCard = 'Bonus'
+            } else {
+                currentCard = `Card ${letterPosition + 1}`
+            }
+        }
+        return(
+            <div className='w-25'>
+                Current Card<br/>
+                <CardFaceDown key='current' display={currentCard}/>
+                <TokensTaken G={G} playerID={playerID}/>
+                { isNextCardAvailable[playerID] && !isClueInProgress && 
+                <div>
+                    {players[playerID].letterPosition >= words[playerID].length-1 ?
+                    <button id = 'bonusCard' onClick = {bonusLetter}>
+                        {players[playerID].letterPosition === words[playerID].length-1 ? 'I know all my letters': 'I know this letter'}
+                    </button>
+                    :
+                    <button id = 'nextCard' onClick = {nextCard}>
+                        Next Card
+                    </button> 
+                    }
+                </div> 
+            }
+            </div>
+        )
+    },[G, gameResults, isClueInProgress, isNextCardAvailable, letterPosition, moves, playerID, players, setOpenModal, words])
+    
+    const knownCards = useMemo(()=>{
     // show pile of cards that are known
-    const cardRow = []
-	for (let i = 0; i < letterPosition && i < G.words[playerID].length; i++) {
-        cardRow.push(
+    const knownCards = []
+	for (let i = 0; i < letterPosition && i < words[playerID].length; i++) {
+        knownCards.push(
             <CardFaceDown 
             key = {`myCard${i}`}
             display = {`Card ${i+1}`} />
         )
 	  }
+      return knownCards
+    },[letterPosition, playerID, words])
+    
 
 return (
     <div className= 'myCards flex flex-row justify-around'>
         <div className= 'w-75'>
             Known Cards<br/>
-            {cardRow}
+            {knownCards}
         </div>
-        <div className='w-25'>
-            Current Card<br/>
-            <CardFaceDown key='current' display={currentCard}/>
-            <TokensTaken G={G} playerID={playerID}/>
-            { G.isNextCardAvailable[playerID] && !G.isClueInProgress && 
-            <div>
-                {G.players[playerID].letterPosition >= G.words[playerID].length-1 ?
-                <button id = 'bonusCard' onClick = {bonusLetter}>
-                    {G.players[playerID].letterPosition === G.words[playerID].length-1 ? 'I know all my letters': 'I know this letter'}
-                </button>
-                :
-                <button id = 'nextCard' onClick = {nextCard}>
-                    Next Card
-                </button> 
-                }
-            </div> 
-        }
-        </div>
+        {current}
     </div>
     )
 }
