45466cebc21a78ed88511f2cdbe267775c02077b
Song Ha <songsongha@gmail.com>
2022-04-03 23:20:39 -0600

    Add validation to Game Setup

--- src/components/Board/GameSetUp/GameSetUp.js
+++ src/components/Board/GameSetUp/GameSetUp.js
@@ -1,36 +1,136 @@
-import React, { useCallback, useEffect }from 'react'
+import React, { useState, useCallback, useEffect }from 'react'
+
+// const GameSetUp = ({wordLength, moves, playerID}) => {
+    
+//     const submitForm = useCallback(() => {
+		
+// 		const name = document.getElementById('txtName').value
+// 		const word = document.getElementById('txtWord').value 
+
+// 		moves.submitWords(playerID, name, word)
+//     },[moves, playerID])
+        
+//     useEffect(() => {
+//         const listener = event => {
+//             if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+//             console.log('Enter key was pressed. Run your function.')
+//             event.preventDefault()
+//             submitForm()
+//             }
+//         }
+//         document.addEventListener('keydown', listener)
+//         return () => {
+//             document.removeEventListener('keydown', listener)
+//         }
+//     }, [submitForm])
+
+//     return ( 
+//         <div className='tc'>
+//             Name: <input type = 'text' id = 'txtName' autoFocus/>
+//             {wordLength !== 'open' ? `${wordLength}-Letter ` : 'Any Length ' }
+//             Word: <input type = 'text' id = 'txtWord'/>
+//             <button id = 'btnSubmit'onClick={submitForm}>Submit</button>
+//         </div>
+//     )
+// }
+// export default GameSetUp
 
 const GameSetUp = ({wordLength, moves, playerID}) => {
-    
-    const submitWord = useCallback(() => {
-		
-		const name = document.getElementById('txtName').value
-		const word = document.getElementById('txtWord').value 
+    const intialValues = { name: '', word: '' }
+    const [formValues, setFormValues] = useState(intialValues)
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
-		moves.submitWords(playerID, name, word)
-    },[moves, playerID])
-        
-    useEffect(() => {
-        const listener = event => {
-            if (event.code === 'Enter' || event.code === 'NumpadEnter') {
-            console.log('Enter key was pressed. Run your function.');
-            event.preventDefault();
-            submitWord()
-            }
-        };
-        document.addEventListener('keydown', listener);
-        return () => {
-            document.removeEventListener('keydown', listener);
+
+    const submitForm = useCallback(() => {
+        const {name, word} = formValues
+		moves.submitWords(playerID, ...name, ...word)
+    },[formValues, moves, playerID])
+  
+   const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormValues({ ...formValues, [name]: value })
+    }
+  
+    const validate = useCallback((values) => {
+        const {name, word} = values
+        let errors = {}
+        if (!name) {
+            errors.name = 'Please enter your name'
+        } else if (name.length > 7) {
+            errors.name = 'Name must be less than 7 characters long'
         }
-    }, [submitWord])
+        if (!word) {
+            errors.word = 'Must enter a word'
+        } else if (word.length !== Number(wordLength)) {
+            errors.word = `Word must be exactly ${wordLength} characters`
+        }
+        // check for real word??
+        return errors
+    },[wordLength])
 
-    return ( 
-        <div className='tc'>
-            Name: <input type = 'text' id = 'txtName' autoFocus/>
-            {wordLength !== 'open' ? `${wordLength}-Letter ` : 'Any Length ' }
-            Word: <input type = 'text' id = 'txtWord'/>
-            <button id = 'btnSubmit'onClick={submitWord}>Submit</button>
+    const handleSubmit =  useCallback(() => {
+        setFormErrors(validate(formValues))
+        setIsSubmitting(true)
+      },[formValues, validate])
+  
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmitting) {
+        submitForm()
+      }
+
+    const listener = event => {
+        if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+        event.preventDefault()
+        handleSubmit()
+        }
+    }
+    document.addEventListener('keydown', listener)
+    return () => {
+        document.removeEventListener('keydown', listener)
+    }
+      
+    }, [formErrors, handleSubmit, isSubmitting, submitForm])
+
+    return (
+        <div className='container tc'>
+          {/* <h1>Sign in to continue</h1>
+          {Object.keys(formErrors).length === 0 && isSubmitting && (
+            <span className='success-msg'>Signed in successfully</span>
+          )} */}
+          <form onSubmit={handleSubmit} noValidate>
+            <div className='form-row'>
+              <label htmlFor='name'>Name : </label>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                value={formValues.name}
+                onChange={handleChange}
+                className={formErrors.name && 'input-error'}
+                autoFocus
+              />
+              {formErrors.name && (
+                <span className='error red'>{formErrors.name}</span>
+              )}
+            </div>
+            <div className='form-row'>
+              <label htmlFor='word'>{wordLength !== 'open' ? `${wordLength}-Letter ` : 'Any Length ' }Word:</label>
+              <input
+                type='word'
+                name='word'
+                id='word'
+                value={formValues.word}
+                onChange={handleChange}
+                className={formErrors.word && 'input-error'}
+              />
+              {formErrors.word && (
+                <span className='error red'>{formErrors.word}</span>
+              )}
+            </div>
+            <button type='submit'>Submit</button>
+          </form>
         </div>
-    )
-}
-export default GameSetUp
+      )
+    }
+    export default GameSetUp
