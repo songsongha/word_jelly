0f810c8b3324966964bb57b65991c9e11fced697
Song Ha <songsongha@gmail.com>
2022-04-09 19:09:19 -0600

    Change fronEndAppBuildPath to ../build

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const server = Server({
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build');
+const frontEndAppBuildPath = path.resolve(__dirname, '../build');
 server.app.use(serve(frontEndAppBuildPath))
 
 server.run(PORT, () => {
